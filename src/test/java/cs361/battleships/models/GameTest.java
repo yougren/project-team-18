63720bd93373306c50a034112eb47ef6b5479037
yougren <eugenemunblit@gmail.com>
2018-10-10 19:26:27 -0700

    Fixed assert imports

--- src/test/java/cs361/battleships/models/GameTest.java
+++ src/test/java/cs361/battleships/models/GameTest.java
@@ -1,6 +1,8 @@
 package cs361.battleships.models;
 
 import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertFalse;
 
 public class GameTest {
@@ -9,20 +11,20 @@ public class GameTest {
     public void testRandomColumn(){
         Game game = new Game();
         int randcol = (int)game.randCol();
-        assert(randcol >= 65 && randcol <= 73);
+        assertTrue(randcol >= 65 && randcol <= 73);
     }
 
     @Test
     public void testRandomRow(){
         Game game = new Game();
         int randrow = game.randRow();
-        assert(randrow >= 1 && randrow <= 10);
+        assertTrue(randrow >= 1 && randrow <= 10);
     }
 
     @Test
     public void testRandomVertical(){
         Game game = new Game();
         boolean vertical = game.randVertical();
-        assert(String.valueOf(vertical).equals("true") || String.valueOf(vertical).equals("false"));
+        assertTrue(String.valueOf(vertical).equals("true") || String.valueOf(vertical).equals("false"));
     }
 }
