1637c817346f53a325b514ba7673f243f24ff255
John Davis <davisjo5@oregonstate.edu>
2018-10-12 12:13:51 -0700

    Added tests for Issue 6 and Issue 26

--- src/test/java/cs361/battleships/models/ResultTest.java
+++ src/test/java/cs361/battleships/models/ResultTest.java
@@ -2,15 +2,33 @@ package cs361.battleships.models;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class ResultTest {
 
     @Test
-    public void testGetResult(){
-        // implement once setResult() has been written
+    public void testGetSetResult(){
+        Result res = new Result();
+        res.setResult(AtackStatus.HIT);
+        AtackStatus ret = res.getResult();
+        assertEquals(AtackStatus.HIT, ret);
     }
 
     @Test
-    public void testGetLocation(){
-        // implement once setLocation() has been written
+    public void testGetSetLocation(){
+        Result res = new Result();
+        Square test = new Square(1, 'A');
+        res.setLocation(test);
+        Square ret = res.getLocation();
+        assertEquals(test, ret);
+    }
+
+    @Test
+    public void testGetSetShip() {
+        Result res = new Result();
+        Ship test = new Ship("DESTROYER");
+        res.setShip(test);
+        Ship ret = res.getShip();
+        assertEquals(test, ret);
     }
 }
