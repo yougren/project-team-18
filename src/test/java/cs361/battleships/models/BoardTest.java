d33ff4461b7d682f94287c2243c354f5c0e3034d
NathanJewell <ncjewell@gmail.com>
2018-10-11 17:21:11 -0700

    Board constructor and tests

--- src/test/java/cs361/battleships/models/BoardTest.java
+++ src/test/java/cs361/battleships/models/BoardTest.java
@@ -2,13 +2,38 @@ package cs361.battleships.models;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
+import static junit.framework.TestCase.*;
 
 public class BoardTest {
 
     @Test
-    public void testInvalidPlacement() {
+    public void testBoardConstructor() {
         Board board = new Board();
-        assertFalse(board.placeShip(new Ship("MINESWEEPER"), 11, 'C', true));
+        assertTrue(board instanceof Board);
+    }
+
+    @Test
+    public void testBoardHeightInitial() {
+        Board board = new Board();
+        assertEquals(board.getHeight(), 10);
+        assertEquals(board.getWidth(), 10);
+    }
+
+    @Test
+    public void testBoardHeightSet() {
+        Board board = new Board();
+        int testHeight = 230;
+        board.setHeight(testHeight);
+        int setHeight = board.getHeight();
+        assertEquals(testHeight, setHeight);
+    }
+
+    @Test
+    public void testBoardWidthSet() {
+        Board board = new Board();
+        int testWidth = 167;
+        board.setWidth(testWidth);
+        int setWidth = board.getWidth();
+        assertEquals(testWidth, setWidth);
     }
 }
