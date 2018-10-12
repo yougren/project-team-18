2d3a44eab06924ab7a2231356479602771d8d846
John Davis <davisjo5@oregonstate.edu>
2018-10-11 19:34:43 -0700

    Issue #6 --> Getters for Result

---
+++ src/test/java/cs361/battleships/models/ResultTest.java
@@ -0,0 +1,16 @@
+package cs361.battleships.models;
+
+import org.junit.Test;
+
+public class ResultTest {
+
+    @Test
+    public void testGetResult(){
+        // implement once setResult() has been written
+    }
+
+    @Test
+    public void testGetLocation(){
+        // implement once setLocation() has been written
+    }
+}
